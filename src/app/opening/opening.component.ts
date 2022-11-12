ece4aa3fd2dfa5f5859fac6aadd93a2a253a64cd
atomheartdani <contact@danielemeregalli.com>
2022-11-12 15:00:12 +0100

    Request login on Opening actions

--- src/app/opening/opening.component.ts
+++ src/app/opening/opening.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Opening } from '@app/@shared/models/opening.model';
+import { AuthenticationGuard } from '@app/auth';
 import { InsertReservationDialogComponent } from './insert-reservation-dialog/insert-reservation-dialog.component';
 import { OpeningDetailDialogComponent } from './opening-detail-dialog/opening-detail-dialog.component';
 
@@ -12,22 +14,31 @@ import { OpeningDetailDialogComponent } from './opening-detail-dialog/opening-de
 export class OpeningComponent implements OnInit {
   @Input() opening: Opening;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private authGuard: AuthenticationGuard,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {}
 
   insertReservation(): void {
-    const dialogRef = this.dialog.open(InsertReservationDialogComponent, { data: this.opening });
-    dialogRef.afterClosed().subscribe((result) => {
-      // TODO reload all openings
-    });
+    if (this.authGuard.canActivate(this.route.snapshot, this.router.routerState.snapshot)) {
+      const dialogRef = this.dialog.open(InsertReservationDialogComponent, { data: this.opening });
+      dialogRef.afterClosed().subscribe((result) => {
+        // TODO reload all openings
+      });
+    }
   }
 
   manage(): void {
-    const dialogRef = this.dialog.open(OpeningDetailDialogComponent, { data: this.opening });
-    dialogRef.afterClosed().subscribe((result) => {
-      // TODO reload all openings
-    });
+    if (this.authGuard.canActivate(this.route.snapshot, this.router.routerState.snapshot)) {
+      const dialogRef = this.dialog.open(OpeningDetailDialogComponent, { data: this.opening });
+      dialogRef.afterClosed().subscribe((result) => {
+        // TODO reload all openings
+      });
+    }
   }
 
   get spaceAvaiable(): boolean {
