8d77ddc3e3a109f1e3ee7e2996b340332dd44f9b
atomheartdani <contact@danielemeregalli.com>
2022-11-12 17:46:46 +0100

    Add generic confirm dialog

---
+++ src/app/@shared/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss'],
+})
+export class ConfirmDialogComponent implements OnInit {
+  confirmAction: string = '';
+  confirmDetail: string = '';
+
+  constructor(
+    private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: { confirmAction: string; confirmDetail: string }
+  ) {
+    this.confirmAction = data.confirmAction;
+    this.confirmDetail = data.confirmDetail;
+  }
+
+  ngOnInit(): void {}
+
+  close(): void {
+    this.dialogRef.close(1);
+  }
+
+  ok(): void {
+    this.dialogRef.close(0);
+  }
+}
--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -6,10 +6,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@app/material.module';
 import { LoaderComponent } from './loader/loader.component';
 import { DateWithMonthNamePipe } from './pipes/dateWithMonthName.pipe';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   imports: [FlexLayoutModule, MaterialModule, TranslateModule, CommonModule],
-  declarations: [DateWithMonthNamePipe, LoaderComponent],
-  exports: [DateWithMonthNamePipe, LoaderComponent],
+  declarations: [DateWithMonthNamePipe, LoaderComponent, ConfirmDialogComponent],
+  exports: [DateWithMonthNamePipe, LoaderComponent, ConfirmDialogComponent],
 })
 export class SharedModule {}
