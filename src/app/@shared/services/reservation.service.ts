4edbef58c01010a16eabd4be81880c9f94b2d96e
atomheartdani <contact@danielemeregalli.com>
2022-11-25 17:07:07 +0100

    Cleanup

--- src/app/@shared/services/reservation.service.ts
+++ src/app/@shared/services/reservation.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Reservation } from '../models/reservation.model';
@@ -15,16 +15,6 @@ export class ReservationService {
 
   getReservations(): Observable<Reservation[]> {
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    /*
-    let params = new HttpParams();
-    params = params.append('page', page.toString());
-    params = params.append('size', size.toString());
-    params = params.append('filters', filters);
-    params = params.append('sort', sort);
-    
-    return this.httpClient.get<Page<Evento>>(`${this.API_URL}/Paginated/`, { headers, params });
-    */
-
     return this.httpClient.get<Reservation[]>(`/reservations/getAll.php`, { headers });
   }
 
