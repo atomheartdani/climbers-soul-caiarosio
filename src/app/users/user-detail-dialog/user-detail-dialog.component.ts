f0a29f2fd1e60b2af4d11c6fa7911327128c4bca
atomheartdani <contact@danielemeregalli.com>
2023-01-07 17:17:28 +0100

    Show user data in dialog

--- src/app/users/user-detail-dialog/user-detail-dialog.component.ts
+++ src/app/users/user-detail-dialog/user-detail-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from '@app/@shared/models/user.model';
 
 @Component({
   selector: 'app-user-detail-dialog',
@@ -6,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail-dialog.component.scss'],
 })
 export class UserDetailDialogComponent implements OnInit {
-  constructor() {}
+  detailForm: FormGroup;
+  user: User;
 
-  ngOnInit(): void {}
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<UserDetailDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: User
+  ) {
+    this.user = data;
+    this.detailForm = fb.group({
+      username: [this.data.username, [Validators.required]],
+      firstname: [this.data.firstname, [Validators.required]],
+      lastname: [this.data.lastname, [Validators.required]],
+      email: [this.data.email, [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.dialogRef.updateSize('50vw', '');
+  }
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UsersComponent } from './users.component';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '@app/@shared';
 import { MaterialModule } from '@app/material.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UserDetailDialogComponent } from './user-detail-dialog/user-detail-dialog.component';
 
 @NgModule({
   declarations: [UsersComponent, UserDetailDialogComponent],
-  imports: [CommonModule, TranslateModule, SharedModule, FlexLayoutModule, MaterialModule, UsersRoutingModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    SharedModule,
+    FlexLayoutModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    UsersRoutingModule,
+  ],
 })
 export class UsersModule {}
