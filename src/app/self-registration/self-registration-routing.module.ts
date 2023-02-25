7ed7db034de3125a431361655a453ba8cd1e3807
atomheartdani <contact@danielemeregalli.com>
2023-02-25 19:54:48 +0100

    Block authenticated users from accessing registration page

--- src/app/self-registration/self-registration-routing.module.ts
+++ src/app/self-registration/self-registration-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 import { SelfRegistrationComponent } from './self-registration.component';
+import { SelfRegistrationGuard } from './self-registration.guard';
 
-const routes: Routes = [{ path: '', component: SelfRegistrationComponent, data: { title: marker('Registrati') } }];
+const routes: Routes = [
+  {
+    path: '',
+    component: SelfRegistrationComponent,
+    data: { title: marker('Registrati') },
+    canActivate: [SelfRegistrationGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [],
+  providers: [SelfRegistrationGuard],
 })
 export class SelfRegistrationRoutingModule {}
---
+++ src/app/self-registration/self-registration.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { CredentialsService } from '@app/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SelfRegistrationGuard implements CanActivate {
+  constructor(private router: Router, private credentialService: CredentialsService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.credentialService.isAuthenticated()) {
+      return false;
+    }
+    return true;
+  }
+}
