e34c438fa89d7ec7bd785f16095cc299c29ea447
atomheartdani <contact@danielemeregalli.com>
2023-03-19 11:06:56 +0100

    Remove FlexLayoutModule from shell module

--- src/app/shell/shell.component.spec.ts
+++ src/app/shell/shell.component.spec.ts
@@ -2,7 +2,6 @@ import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@app/material.module';
 
 import { AuthenticationService, CredentialsService } from '@app/auth';
@@ -19,14 +18,7 @@ describe('ShellComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [
-        TranslateModule.forRoot(),
-        I18nModule,
-        BrowserAnimationsModule,
-        FlexLayoutModule,
-        MaterialModule,
-        RouterTestingModule,
-      ],
+      imports: [TranslateModule.forRoot(), I18nModule, BrowserAnimationsModule, MaterialModule, RouterTestingModule],
       providers: [
         { provide: AuthenticationService, useClass: MockAuthenticationService },
         { provide: CredentialsService, useClass: MockCredentialsService },
--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { AuthModule } from '@app/auth';
 import { I18nModule } from '@app/i18n';
@@ -10,7 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { ShellComponent } from './shell.component';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, FlexLayoutModule, MaterialModule, AuthModule, I18nModule, RouterModule],
+  imports: [CommonModule, TranslateModule, MaterialModule, AuthModule, I18nModule, RouterModule],
   declarations: [HeaderComponent, ShellComponent],
 })
 export class ShellModule {}
