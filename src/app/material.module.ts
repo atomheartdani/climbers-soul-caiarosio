f3d59c5371f0f14f4e3dea8416480d2eb7e1b925
atomheartdani <contact@danielemeregalli.com>
2024-10-16 20:22:13 +0200

    Remove unused material modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,8 +7,6 @@
  */
 
 import { NgModule } from '@angular/core';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
@@ -18,7 +16,6 @@ import {
   DateAdapter,
   MAT_DATE_FORMATS,
   MatCommonModule,
-  MatLineModule,
   MatOptionModule,
   MatPseudoCheckboxModule,
   MatRippleModule,
@@ -26,15 +23,12 @@ import {
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
@@ -42,13 +36,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatSortModule } from '@angular/material/sort';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatTreeModule } from '@angular/material/tree';
 import { IsoDateAdapter } from './@shared/iso-date-adapter';
 
 export const CUSTOM_DATE_FORMATS = {
@@ -65,8 +55,6 @@ export const CUSTOM_DATE_FORMATS = {
 
 @NgModule({
   exports: [
-    MatAutocompleteModule,
-    MatBadgeModule,
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
@@ -76,17 +64,13 @@ export const CUSTOM_DATE_FORMATS = {
     MatDatepickerModule,
     MatDialogModule,
     MatDividerModule,
-    MatExpansionModule,
     MatFormFieldModule,
-    MatGridListModule,
     MatIconModule,
     MatInputModule,
-    MatLineModule,
     MatListModule,
     MatMenuModule,
     MatOptionModule,
     MatPaginatorModule,
-    MatProgressBarModule,
     MatProgressSpinnerModule,
     MatPseudoCheckboxModule,
     MatRadioModule,
@@ -96,13 +80,9 @@ export const CUSTOM_DATE_FORMATS = {
     MatSlideToggleModule,
     MatSliderModule,
     MatSnackBarModule,
-    MatSortModule,
     MatStepperModule,
     MatTableModule,
-    MatTabsModule,
     MatToolbarModule,
-    MatTooltipModule,
-    MatTreeModule,
   ],
   providers: [
     { provide: DateAdapter, useClass: IsoDateAdapter },
