e4d68138e252428d27c5104503a8e7fff51dfd17
atomheartdani <contact@danielemeregalli.com>
2025-03-23 13:44:24 +0100

    Remove unnused files since 68c75128fe0dda59545bc51041cc2aa7713700d3

--- src/app/self-registration/self-registration.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SelfRegistrationRoutingModule } from './self-registration-routing.module';
-
-@NgModule({
-  imports: [SelfRegistrationRoutingModule],
-})
-export class SelfRegistrationModule {}
