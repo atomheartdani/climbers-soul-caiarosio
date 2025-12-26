9475e3561e86e22a99f9c155e8a5cac8aeb0d0a8
atomheartdani <contact@danielemeregalli.com>
2025-12-26 12:08:56 +0100

    Update to angular 21

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@
  * For app-specific initialization, use `app/app.component.ts`.
  */
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from '@app/app.component';
 import { appConfig } from '@app/app.config';
@@ -14,4 +14,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((err) => console.error(err));
